5ebac346363104dbe60a086db1de64cd3440aad0
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-01-15 12:12:03 -0500

    chore: init commit

---
+++ platform_plugin_elm_credentials/__init__.py
@@ -0,0 +1,5 @@
+"""
+Open edx plugin that includes API to generate JSON files in ELMv3
+"""
+
+__version__ = '0.1.0'
---
+++ platform_plugin_elm_credentials/apps.py
@@ -0,0 +1,13 @@
+"""
+platform_plugin_elm_credentials Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class PlatformPluginElmCredentialsConfig(AppConfig):
+    """
+    Configuration for the platform_plugin_elm_credentials Django application.
+    """
+
+    name = 'platform_plugin_elm_credentials'
